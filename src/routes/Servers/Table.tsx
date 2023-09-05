1ece8ef7fef70364527652078d564ecc3b6f1882
Ilya Hamisoniya <Ildoradogam@gmail.com>
2023-09-05 12:04:36 +0200

    fix: table scrolling when the width is still sufficient

--- src/routes/Servers/Table.tsx
+++ src/routes/Servers/Table.tsx
@@ -62,7 +62,7 @@ export const Table = <T extends Record<string, unknown>>({
 
   return (
     <div className="flex flex-col overflow-x-auto">
-      <div className="sm:-mx-6 lg:-mx-8">
+      <div>
         <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
           <div className="overflow-x-auto">
             <table className="min-w-full text-left text-sm font-light">
